d8ae4c8cfe2357e35dea7385e26b150e14cf3f2b
Wyatt <wyatt.furois@gmail.com>
2025-07-02 11:08:33 -0500

    feat: hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col h-screen justify-between">
+        <header className="text-4xl text-sky-300 font-bold p-10 w-full bg-zinc-700">
+          <a href="/" target="_blank">
+            Bento Tools
+          </a>
+        </header>
+        <main className="w-full mb-auto bg-zinc-500">
+          <div className="w-9/12 m-auto flex">
+            <div className="text-white text-2xl p-4 border-2">
+              hello I am a tool :)
+            </div>
+          </div>
+        </main>
+        <footer className="text-white text-right p-5 w-full bg-zinc-700">
+          Created by <a href="https://www.github.com/tr1ptychs">tr1ptychs</a>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+} else {
+  console.error("Element with id 'root' not found in the document.");
+}
