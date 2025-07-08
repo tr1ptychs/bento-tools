1e1fc864d205971103cb691ae5adaf29657139e4
Wyatt <wyatt.furois@gmail.com>
2025-07-08 13:54:10 -0500

    feat(tools): added wrapper for tool styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Tool from "./Tool.tsx";
 import Stopwatch from "./Stopwatch.tsx";
 import Timer from "./Timer.tsx";
 
@@ -13,12 +14,12 @@ function App() {
         </header>
         <main className="w-full mb-auto h-full bg-zinc-500">
           <div className="w-9/12 m-auto h-full flex">
-            <div className="text-white text-2xl p-4 border-2 m-auto">
+            <Tool>
               <Stopwatch />
-            </div>
-            <div className="text-white text-2xl p-4 border-2 m-auto">
+            </Tool>
+            <Tool>
               <Timer />
-            </div>
+            </Tool>
           </div>
         </main>
         <footer className="text-white text-right p-5 w-full bg-zinc-700">
---
+++ src/Tool.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Tool = ({ children }) => {
+  return (
+    <div className="text-white text-2xl p-4 border-2 m-auto">{children}</div>
+  );
+};
+
+export default Tool;
