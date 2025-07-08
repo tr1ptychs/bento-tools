59ae563b0e0106623a3a8fcf4c343d74f82747bb
Wyatt <wyatt.furois@gmail.com>
2025-07-08 14:02:13 -0500

    fix(tools): typescript bug (no props)

--- src/Tool.tsx
+++ src/Tool.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Tool = ({ children }) => {
+const Tool = ({ children }: { children: React.reactNode }) => {
   return (
     <div className="text-white text-2xl p-4 border-2 m-auto">{children}</div>
   );
